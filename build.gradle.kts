aa39a83b5d8c03f04bfdc6096618255985ad03a3
Sergey Okatov <svok@users.noreply.github.com>
2023-07-01 12:39:09 +0500

    m1l1 обновление версий (#1)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.7.21" apply false
+    kotlin("jvm") apply false
 }
 
 group = "ru.otus.otuskotlin.marketplace"
-version = "1.0-SNAPSHOT"
+version = "0.0.1"
 
 repositories {
     mavenCentral()
@@ -19,6 +19,6 @@ subprojects {
     }
 
     tasks.withType<KotlinCompile> {
-        kotlinOptions.jvmTarget = "11"
+        kotlinOptions.jvmTarget = "17"
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,12 @@
-rootProject.name = "ok-marketplace-202212"
+rootProject.name = "ok-marketplace-202306"
+
+pluginManagement {
+    plugins {
+        val kotlinVersion: String by settings
+
+        kotlin("jvm") version kotlinVersion apply false
+
+    }
+}
 
 include("m1l1-quickstart")
