73ab7354a46720b3a547706dd0aeb4f3af33c43b
Sergey Okatov <sokatov@gmail.com>
2023-08-06 11:30:13 +0500

    m2l3 merge with 202212

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,12 +9,12 @@ pluginManagement {
 }
 
 
-include("m1l1-quickstart")
-include("m1l2-basic")
-include("m1l3-oop")
-include("m1l4-dsl")
-include("m1l5-coroutines")
-include("m1l6-flows")
-include("m1l7-kmp")
+//include("m1l1-quickstart")
+//include("m1l2-basic")
+//include("m1l3-oop")
+//include("m1l4-dsl")
+//include("m1l5-coroutines")
+//include("m1l6-flows")
+//include("m1l7-kmp")
 
 include("ok-marketplace-acceptance")
