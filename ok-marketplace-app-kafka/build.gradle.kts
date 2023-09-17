8338c6680349d9a9c102e9adb683dc70ad145d9f
Sergey Okatov <sokatov@gmail.com>
2023-09-17 15:52:37 +0500

    m3l6 - kafka-app

---
+++ ok-marketplace-app-kafka/build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    application
+    kotlin("jvm")
+}
+
+application {
+    mainClass.set("ru.otus.otuskotlin.marketplace.app.kafka.MainKt")
+}
+
+dependencies {
+    val kafkaVersion: String by project
+    val coroutinesVersion: String by project
+    val atomicfuVersion: String by project
+    val logbackVersion: String by project
+    val kotlinLoggingJvmVersion: String by project
+    implementation("org.apache.kafka:kafka-clients:$kafkaVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
+    implementation("org.jetbrains.kotlinx:atomicfu:$atomicfuVersion")
+
+    // log
+    implementation("ch.qos.logback:logback-classic:$logbackVersion")
+    implementation("io.github.microutils:kotlin-logging-jvm:$kotlinLoggingJvmVersion")
+
+    // transport models
+    implementation(project(":ok-marketplace-common"))
+    implementation(project(":ok-marketplace-api-v1-jackson"))
+    implementation(project(":ok-marketplace-api-v2-kmp"))
+    implementation(project(":ok-marketplace-mappers-v1"))
+    implementation(project(":ok-marketplace-mappers-v2"))
+    // logic
+    implementation(project(":ok-marketplace-biz"))
+
+    testImplementation(kotlin("test-junit"))
+}
